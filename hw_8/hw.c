481676436b8df0e978015b777899dee289f7e6c7
Ryan Pascal <ryanpascal99@yahoo.com>
2018-10-04 13:50:54 -0400

    adding hw problem 8

---
+++ hw_8/hw.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("β = 1011 0101 0111 1000 1111 0000 0000 0000\n");
+    printf("Hex value: 0xB578F000\n");
+    printf("True Decimal Representation: 3044601856\n");
+
+    unsigned long unsignedLong = 0xB578F000;
+    printf("unsigned long: %lu\n", unsignedLong);
+
+    long Long = 0xB578F000;
+    printf("long: %lu\n", Long);
+
+    unsigned int unsignedInt = 0xB578F000;
+    printf("unsigned int: %u\n", unsignedInt);
+
+    unsigned short int unsignedShortInt = 0xB578F000;
+    printf("unsigned short int: %u\n", unsignedShortInt);
+
+    short int shortInt = 0xB578F000;
+    printf("short int: %i\n", shortInt);
+
+    int Int = 0xB578F000;
+    printf("int: %i\n", Int);
+
+    float Float = 0xB578F000;
+    printf("float: %f\n", Float);
+
+    double Double = 0xB578F000;
+    printf("double: %f\n", Double);
+
+
+}
